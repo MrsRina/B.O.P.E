f540254d87eaa992054c0bd64a0f079393b3a792
Wks JanStudio <37088203+SirRina@users.noreply.github.com>
2020-04-24 15:04:53 -0300

    BopeBind working gay, but I need place none.
    I need update something

--- B.O.P.E/src/main/java/rina/turok/bope/bopemod/commands/BopeBind.java
+++ B.O.P.E/src/main/java/rina/turok/bope/bopemod/commands/BopeBind.java
@@ -23,32 +23,39 @@ public class BopeBind extends BopeCommand {
 	}
 
 	public boolean get_message(String[] message) {
-		if (message.length > 2) {
-			String module = "null;";
-			String key    = "null";
+		String module = "null;";
+		String key    = "null";
 
+		if (message.length > 1) {
 			module = message[1];
-			key    = message[2];
+		}
 
-			if (module.equals("null")) {
-				return true;
-			}
+		if (message.length > 2) {
+			key = message[2];
+		}
 
-			if (key.equals("null")) {
-				return true;
-			}
+		if (module.equals("null")) {
+			BopeMessage.send_client_error_message(current_prefix() + "bind <ModuleNameNoSpace> <key>");
 
-			BopeModule module_requested = Bope.get_module_manager().get_module_with_tag(module);
+			return true;
+		}
 
-			if (module_requested != null) {
-				int new_bind = Keyboard.getKeyIndex(key.toLowerCase());
+		if (key.equals("null")) {
+			BopeMessage.send_client_error_message(current_prefix() + "bind <ModuleNameNoSpace> <key>");
 
-				module_requested.set_bind(new_bind);
+			return true;
+		}
 
-				BopeMessage.send_client_message("The bind of module " + module_requested.get_name() + " is now " + module_requested.get_bind("0"));
-			} else {
-				BopeMessage.send_client_error_message("It key not exist.");
-			}
+		BopeModule module_requested = Bope.get_module_manager().get_module_with_tag(module);
+
+		if (module_requested != null) {
+			int new_bind = Keyboard.getKeyIndex(key.toUpperCase());
+
+			module_requested.set_bind(new_bind);
+
+			BopeMessage.send_client_message("The bind of module " + module_requested.get_name() + " is now " + module_requested.get_bind("0"));
+		} else {
+			BopeMessage.send_client_error_message("It key not exist.");
 		}
 
 		return true;

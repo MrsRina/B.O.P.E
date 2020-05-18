ae670c01893e2ac5b39904b60b51e5738960e220
Wks JanStudio <37088203+SirRina@users.noreply.github.com>
2020-05-17 23:45:49 -0300

    Added animation into PushMiner

--- B.O.P.E/src/main/java/rina/turok/turok/draw/TurokRenderHelp.java
+++ B.O.P.E/src/main/java/rina/turok/turok/draw/TurokRenderHelp.java
@@ -45,7 +45,7 @@ public class TurokRenderHelp extends Tessellator {
 		TurokGL.release();
 	}
 
-	public static void draw_cube(BlockPos pos, float r, float g, float b, float a) {
+	public static void draw_cube(BlockPos pos, float r, float g, float b, float a, String faces) {
 		BufferBuilder buffer = INSTANCE.getBuffer();
 
 		float w = 1;
@@ -56,34 +56,46 @@ public class TurokRenderHelp extends Tessellator {
 		float y = pos.y;
 		float z = pos.z;
 
-		buffer.pos(x + w, y, z).color(r, g, b, a).endVertex();
-		buffer.pos(x + w, y, z + d).color(r, g, b, a).endVertex();
-		buffer.pos(x, y, z + d).color(r, g, b, a).endVertex();
-		buffer.pos(x, y, z).color(r, g, b, a).endVertex();
+		if (((boolean) Arrays.asList(type.split("-")).contains("down"))) {
+			buffer.pos(x + w, y, z).color(r, g, b, a).endVertex();
+			buffer.pos(x + w, y, z + d).color(r, g, b, a).endVertex();
+			buffer.pos(x, y, z + d).color(r, g, b, a).endVertex();
+			buffer.pos(x, y, z).color(r, g, b, a).endVertex();
+		}
 
-		buffer.pos(x + w, y + h, z).color(r, g, b, a).endVertex();
-		buffer.pos(x, y + h, z).color(r, g, b, a).endVertex();
-		buffer.pos(x, y + h, z + d).color(r, g, b, a).endVertex();
-		buffer.pos(x + w, y + h, z + d).color(r, g, b, a).endVertex();
+		if (((boolean) Arrays.asList(type.split("-")).contains("up"))) {
+			buffer.pos(x + w, y + h, z).color(r, g, b, a).endVertex();
+			buffer.pos(x, y + h, z).color(r, g, b, a).endVertex();
+			buffer.pos(x, y + h, z + d).color(r, g, b, a).endVertex();
+			buffer.pos(x + w, y + h, z + d).color(r, g, b, a).endVertex();
+		}
 
-		buffer.pos(x + w, y, z).color(r, g, b, a).endVertex();
-		buffer.pos(x, y, z).color(r, g, b, a).endVertex();
-		buffer.pos(x, y + h, z).color(r, g, b, a).endVertex();
-		buffer.pos(x + w, y + h, z).color(r, g, b, a).endVertex();
+		if (((boolean) Arrays.asList(type.split("-")).contains("north"))) {
+			buffer.pos(x + w, y, z).color(r, g, b, a).endVertex();
+			buffer.pos(x, y, z).color(r, g, b, a).endVertex();
+			buffer.pos(x, y + h, z).color(r, g, b, a).endVertex();
+			buffer.pos(x + w, y + h, z).color(r, g, b, a).endVertex();
+		}
 
-		buffer.pos(x, y, z).color(r, g, b, a).endVertex();
-		buffer.pos(x, y, z + d).color(r, g, b, a).endVertex();
-		buffer.pos(x, y + h, z + d).color(r, g, b, a).endVertex();
-		buffer.pos(x, y + h, z).color(r, g, b, a).endVertex();
+		if (((boolean) Arrays.asList(type.split("-")).contains("south"))) {
+			buffer.pos(x, y, z).color(r, g, b, a).endVertex();
+			buffer.pos(x, y, z + d).color(r, g, b, a).endVertex();
+			buffer.pos(x, y + h, z + d).color(r, g, b, a).endVertex();
+			buffer.pos(x, y + h, z).color(r, g, b, a).endVertex();
+		}
 
-		buffer.pos(x, y, z).color(r, g, b, a).endVertex();
-		buffer.pos(x, y, z + d).color(r, g, b, a).endVertex();
-		buffer.pos(x, y + h, z + d).color(r, g, b, a).endVertex();
-		buffer.pos(x, y + h, z).color(r, g, b, a).endVertex();
+		if (((boolean) Arrays.asList(type.split("-")).contains("west"))) {
+			buffer.pos(x, y, z).color(r, g, b, a).endVertex();
+			buffer.pos(x, y, z + d).color(r, g, b, a).endVertex();
+			buffer.pos(x, y + h, z + d).color(r, g, b, a).endVertex();
+			buffer.pos(x, y + h, z).color(r, g, b, a).endVertex();
+		}
 
-		buffer.pos(x + w, y, z + d).color(r, g, b, a).endVertex();
-		buffer.pos(x + w, y, z).color(r, g, b, a).endVertex();
-		buffer.pos(x + w, y + h, z).color(r, g, b, a).endVertex();
-		buffer.pos(x + w, y + h, z + d).color(r, g, b, a).endVertex();
+		if (((boolean) Arrays.asList(type.split("-")).contains("east"))) {
+			buffer.pos(x + w, y, z + d).color(r, g, b, a).endVertex();
+			buffer.pos(x + w, y, z).color(r, g, b, a).endVertex();
+			buffer.pos(x + w, y + h, z).color(r, g, b, a).endVertex();
+			buffer.pos(x + w, y + h, z + d).color(r, g, b, a).endVertex();
+		}
 	}
 }
